34d258654ac90abe9d527f22f46a2103984aa0c2
Tyler Nichols <tegulator98@gmail.com>
2019-06-17 19:03:37 -0600

    Got Jquery and ajax working.

--- javascript/ajax.js
+++ javascript/ajax.js
@@ -1,18 +1,25 @@
 // Api with key and paramaters https://api.giphy.com/v1/gifs/search?api_key=v5udxlrA4xygbwK590k94UltZ3rV6ohA&q=cats&limit=10&offset=0&rating=PG&lang=en
+$(document).ready(function(){
+
 
 
 //on click function
-$("#find-gif").on("click", function(){
-    //Grab the User input from the search bar
-    var userInput = $("#gif-input").val();
+$("#find-gif").on("click", function(event){
+    event.preventDefault();
+
+//Grab the User input from the search bar
+var userInput = $("#gif-input").val();
 //put that into our api request
-var queryUrl = "https://api.giphy.com/v1/gifs/search?api_key=v5udxlrA4xygbwK590k94UltZ3rV6ohA&q="+ userInput + "&limit=10&offset=0&rating=PG&lang=en"
+var queryUrl = "https://api.giphy.com/v1/gifs/search?api_key=v5udxlrA4xygbwK590k94UltZ3rV6ohA&q="+ userInput +"&limit=10&offset=0&rating=PG&lang=en"
 //ajax function
 $.ajax({
     url: queryUrl,
     method: "GET"
-})
-//get the result and log it
+}).then(function(response) {
+    //get the result and log it
+    console.log(response);
+});
+
 
 // grab the gifs urls from the response
 
@@ -24,5 +31,7 @@ $.ajax({
     
 
     
+    });
+
 });
 
